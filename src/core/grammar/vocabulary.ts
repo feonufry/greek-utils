ce2b30ebcffd9781a14d6071d24e2498b57a14c9
Roman Liberov <r.liberov@gmail.com>
2019-07-28 11:00:09 +0300

    Improve vocabulary

--- src/core/grammar/vocabulary.ts
+++ src/core/grammar/vocabulary.ts
@@ -8,3 +8,43 @@ vocabulary("eita", GrammarType.ADVERB, {
 vocabulary("wra", GrammarType.NOUN, {
     primary: "час"
 }, false);
+
+vocabulary("proc", GrammarType.PREPOSITION, {
+    primary: "около, при",
+    secondary: "+ Твор.падеж"
+}, false);
+
+vocabulary("proc", GrammarType.ADVERB, {
+    primary: "а также, к тому же, сверх того",
+    secondary: "πρός δὲ, πρός δὲ καὶ, καὶ πρός"
+}, false);
+
+vocabulary("proc", GrammarType.PREPOSITION, {
+    primary: "к, по направлению к, в, на [движение или положение]",
+    secondary: "+ Твор.падеж"
+}, false);
+
+vocabulary("varqhki", GrammarType.NOUN, {
+    primary: "нартекс (притвор)"
+}, false);
+
+vocabulary("vaoc", GrammarType.NOUN, {
+    primary: "храм"
+}, false);
+
+vocabulary("diakovoc", GrammarType.NOUN, {
+    primary: "диакон"
+}, false);
+
+vocabulary("pavtwv", GrammarType.NOUN, {
+    primary: "всем",
+    secondary: "Род.п.,мн.ч.,муж.р,ср.р - all, every, each"
+}, false);
+
+vocabulary("hsuхh/", GrammarType.ADVERB, {
+    primary: "тихо, спокойно, неторопливо"
+}, true);
+
+vocabulary("ekklhsiarхhc", GrammarType.ADVERB, {
+    primary: "экклесиарх"
+}, true);
