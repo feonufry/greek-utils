349badb264bd9ae90b98c6d78efff69d1a3f56e3
Roman Liberov <r.liberov@gmail.com>
2019-07-29 10:59:39 +0300

    Extend vocabulary

--- src/core/grammar/vocabulary.ts
+++ src/core/grammar/vocabulary.ts
@@ -1,53 +1,9 @@
 import { vocabulary } from "./registry";
 import { GrammarType } from "./index";
 
-vocabulary("eita", GrammarType.ADVERB, {
-    primary: "потом, затем, далее, после"
-});
-
-vocabulary("wra", GrammarType.NOUN, {
-    primary: "час"
-}, false);
-
-vocabulary("proc", GrammarType.PREPOSITION, {
-    primary: "около, при",
-    secondary: "+ Твор.падеж"
-}, false);
-
-vocabulary("proc", GrammarType.ADVERB, {
-    primary: "а также, к тому же, сверх того",
-    secondary: "πρός δὲ, πρός δὲ καὶ, καὶ πρός"
-}, false);
-
-vocabulary("proc", GrammarType.PREPOSITION, {
-    primary: "к, по направлению к, в, на [движение или положение]",
-    secondary: "+ Твор.падеж"
-}, false);
-
-vocabulary("varqhki", GrammarType.NOUN, {
-    primary: "нартекс (притвор)"
-}, false);
-
-vocabulary("vaoc", GrammarType.NOUN, {
-    primary: "храм"
-}, false);
-
-vocabulary("diakovoc", GrammarType.NOUN, {
-    primary: "диакон"
-}, false);
-
-vocabulary("pavtwv", GrammarType.NOUN, {
-    primary: "всем",
-    secondary: "Род.п.,мн.ч.,муж.р,ср.р - all, every, each"
-}, false);
-
-vocabulary("hsuхh/", GrammarType.ADVERB, {
-    primary: "тихо, спокойно, неторопливо"
-}, true);
-
-vocabulary("ekklhsiarхhc", GrammarType.NOUN, {
-    primary: "экклесиарх"
-}, false);
+//-------------------------------------------------------------------------------------------------------------------
+// BASE
+//-------------------------------------------------------------------------------------------------------------------
 
 vocabulary("ego", GrammarType.PARTICLE, {
     primary: "я"
@@ -94,3 +50,89 @@ vocabulary("auta", GrammarType.PARTICLE, {
     primary: "они - сред.род",
     secondary: "в функции личного употребляется в косвенных падежах"
 }, false);
+
+//-------------------------------------------------------------------------------------------------------------------
+// EXTENDED
+//-------------------------------------------------------------------------------------------------------------------
+
+vocabulary("proc", GrammarType.PREPOSITION, {
+    primary: "около, при",
+    secondary: "+ Твор.падеж"
+}, false);
+
+vocabulary("proc", GrammarType.ADVERB, {
+    primary: "а также, к тому же, сверх того",
+    secondary: "πρός δὲ, πρός δὲ καὶ, καὶ πρός"
+}, false);
+
+vocabulary("proc", GrammarType.PREPOSITION, {
+    primary: "к, по направлению к, в, на [движение или положение]",
+    secondary: "+ Твор.падеж"
+}, false);
+
+vocabulary("htoi", GrammarType.CONJUNCTION, {
+    primary: "или"
+}, false);
+
+vocabulary("eita", GrammarType.ADVERB, {
+    primary: "потом, затем, далее, после"
+});
+
+
+//-------------------------------------------------------------------------------------------------------------------
+// CUSTOM
+//-------------------------------------------------------------------------------------------------------------------
+
+vocabulary("wra", GrammarType.NOUN, {
+    primary: "час"
+}, false);
+
+vocabulary("varqhki", GrammarType.NOUN, {
+    primary: "нартекс (притвор)"
+}, false);
+
+vocabulary("vaoc", GrammarType.NOUN, {
+    primary: "храм"
+}, false);
+
+vocabulary("diakovoc", GrammarType.NOUN, {
+    primary: "диакон"
+}, false);
+
+vocabulary("pavtwv", GrammarType.NOUN, {
+    primary: "всем",
+    secondary: "Род.п.,мн.ч.,муж.р,ср.р - all, every, each"
+}, false);
+
+vocabulary("hsuхh/", GrammarType.ADVERB, {
+    primary: "тихо, спокойно, неторопливо"
+}, true);
+
+vocabulary("ekklhsiarхhc", GrammarType.NOUN, {
+    primary: "экклесиарх"
+}, false);
+
+vocabulary("akolouqia", GrammarType.NOUN, {
+    primary: "последование"
+}, false);
+
+vocabulary("suvaxariov", GrammarType.NOUN, {
+    primary: "синаксарь"
+}, false);
+
+vocabulary("tupikov", GrammarType.NOUN, {
+    primary: "типикон"
+}, false);
+
+vocabulary("movh", GrammarType.NOUN, {
+    primary: "монастырь"
+}, false);
+
+vocabulary("uperagia", GrammarType.ADJECTIVE, {
+    primary: "Пресвятая"
+}, false);
+
+vocabulary("qeotokoc", GrammarType.NOUN, {
+    primary: "Богородица"
+}, false);
+
